283137de108fed7050bbd737936e80418ae3f67f
Jason Waring <jason.waring@softwaringsolutions.com>
2025-01-18 09:00:06 +0800

    Updated to use sass library rather than node-sass

--- scripts/install.js
+++ scripts/install.js
@@ -1,6 +1,4 @@
-var fs = require('fs'),
-    chalk = require('chalk'),
-    path = require('path');
+  var fs = require('fs'), path = require('path');
 
 function install(root) {
     var appDir = path.join(root, 'app'),
@@ -13,12 +11,12 @@ function install(root) {
     if (fs.existsSync(appDir)) {
         if (!fs.existsSync(dst)) {
             fs.writeFileSync(dst, fs.readFileSync(src));
-            console.log(chalk.green('Hook was successfully installed'));
+            console.log('Hook was successfully installed');
         } else {
             var content = fs.readFileSync(dst, {encoding: 'utf8'});
             if (!/function stss/.test(content)) {
                 fs.appendFileSync(dst, fs.readFileSync(src));
-                console.log(chalk.green('Hook was successfully installed'));
+                console.log('Hook was successfully installed');
             } else {
                 try {
                     var reFunction = /function stss\(config, logger\) { \/\/--v\d+\.\d+\.\d+([\s\S]+?)^}$/gm,
@@ -30,20 +28,20 @@ function install(root) {
                         installedVersionNr = (installedVersion[0]*1000 + installedVersion[1])*1000 + installedVersion[2];
 
                     if (installedVersionNr >= newVersionNr) {
-                        console.log(chalk.green('Hook was already installed'));
+                        console.log('Hook was already installed');
                     } else {
                         newSource = newSource.match(reFunction)[0];
                         content = content.replace(reFunction, newSource);
                         fs.writeFileSync(dst, content);
-                        console.log(chalk.green('Hook was successfully updated to v'+newVersion.join('.')));
+                        console.log('Hook was successfully updated to v'+newVersion.join('.'));
                     }
                 } catch(e) {
-                    console.log(chalk.red('Hook could not be updated to v'+newVersion.join('.') + ' automatically, please update it manually'));
+                    console.log('ERROR: Hook could not be updated to v'+newVersion.join('.') + ' automatically, please update it manually');
                 }
             }
         }
     } else {
-        console.log(chalk.red('Could not find the "app"-folder in working directory ('+root+').\n') + chalk.red.bold('The pre-compile-hook was not installed!'));
+        console.log('ERROR: Could not find the "app"-folder in working directory ('+root+').\n') + chalk.red.bold('The pre-compile-hook was not installed!');
     }
 }
 
